5fe676af92c50bc26f6ce665cae3fb49e44dc67d
Daniel González <dgonzalez@iessarenal.net>
2025-04-04 09:30:27 +0200

    Fixed no compiling errors

--- reflex_project/pages/show_product.py
+++ reflex_project/pages/show_product.py
@@ -2,7 +2,22 @@ import reflex as rx
 
 def show_product_page() -> rx.Component:
     return rx.container(
-        foreach_table_example(),
+        # foreach_table_example(),
+        
+        # ! This is a placeholder for the actual implementation
+        rx.vstack(
+            rx.heading("Show Products", size="9"),
+            rx.hstack(
+                rx.text("Product Name", size="4"),
+                rx.text("Product Price", size="4"),
+                rx.text("Product Description", size="4"),
+                spacing="2",
+            ),
+            spacing="8",
+            justify="center",
+            align="center",
+            min_height="85vh",
+        )
     )
 
 
@@ -27,9 +42,9 @@ def foreach_table_example():
             ),
         ),
         rx.table.body(
-            # rx.foreach(
-            #     # ! Replace with your data source
-            # )
+            rx.foreach(
+                # ! Replace with your data source
+            )
         ),
         width="100%",
     )
